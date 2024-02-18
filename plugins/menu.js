d9abd347cd1a3b3ae69012d279a658b4a48f5899
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2024-02-18 21:00:36 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -28,7 +28,7 @@ const defaultMenu = {
 ⏰ *Uptime:* %muptime  
 ┗━━━━━━━━━━⬣
 %readmore
-  ≡ *B O B I Z A | M E N U*
+  ≡ *O B I T O | M E N U*
 `.trimStart(),
   header: '┏━━⊜ *_%category_* ',
   body: '┃⋄ %cmd %isdiamond %isPremium',
@@ -136,10 +136,10 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 text: text,
 contextInfo: {
 externalAdReply: {
-title: 'BOBIZA BOT ♥',
-body: "أول بوت واتساب في العالم العربي 💖",
-thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
-sourceUrl: 'https://instagram.com/noureddine_ouafy',
+title: 'OBITO BOT ♥',
+body: "أول بوت واتساب في العالم العربي من قبل ايوب💖",
+thumbnailUrl: 'https://telegra.ph/file/de8bc57c61836c8e4316b.png',
+sourceUrl: 'https://instagram.com/AYOUB_OTAKUM',
 mediaType: 1,
 renderLargerThumbnail: true
 }}}, { quoted: m})
