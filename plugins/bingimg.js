d17dc235d6b18080ff1ab52a8e2e75cb4784156f
aayoubayoub2781 <148594510+aayoubayoub2781@users.noreply.github.com>
2024-02-20 17:45:05 +0100

    Update bingimg.js

--- plugins/bingimg.js
+++ plugins/bingimg.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     m.reply("تابع صاحب البوت في حساباته تشجيعا له ♥...\ninstagram.com/ayoub_otakum")
 
     try {
-        let response = await fetch(`https://api.maelyn.my.id/api/bingimage?prompt=${text}&apikey=ep5roHTBxt`)
+        let response = await fetch(`https://api.maelyn.my.id/api/bingimage?prompt=&apikey=ep5roHTBxt`)
         let result = await response.json()
 
         if (result.status === "Success" && result.result.length > 0) {
